882f7d4a80c14a9c8cacf2d8a915e9b42eb15109
Thamizh Learner <NaveenLearnsThamizh@gmail.com>
2025-11-16 10:22:31 +0530

    Letter sample updated

--- Letters/letter.go
+++ Letters/letter.go
@@ -7,12 +7,16 @@ import (
 )
 
 func main() {
-	string2letters("தமிழ்")
+	ustr := "தமிழ்"
+	fmt.Printf("Given Unicode string: %s\n", ustr)
+	string2letters(ustr)
+
+	fmt.Println("\nLetter split")
+	letterAnalysis(ustr)
 }
 
 // Get each Thamizh letter from Thamizh string
 func string2letters(ustr string) {
-	fmt.Printf("Given Unicode string: %s\n", ustr)
 
 	str := script.MustDecode(ustr)
 	fmt.Printf("Thamizh string      : %s\n", str)
@@ -20,5 +24,18 @@ func string2letters(ustr string) {
 	fmt.Println("Thamizh letters forming the Thamizh string:")
 	for letter := range str.Values() {
 		fmt.Println(letter)
+	}
+}
+
+func letterAnalysis(ustr string) {
+	str := script.MustDecode(ustr)
+
+	for letter := range str.Values() {
+		fmt.Print(letter)
+		if letter.IsCV() {
+			consonant, vowel := letter.DetachedCV()
+			fmt.Printf(" = %v + %v", consonant, vowel)
+		}
+		fmt.Println()
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ThamizhLearner/ThamizhLibUsage
 
 go 1.25.1
 
-require github.com/ThamizhLearner/Thamizh v0.0.0-20251112103136-b84d94c049fa
+require github.com/ThamizhLearner/Thamizh v0.0.0-20251116042520-e507b8a9c305
